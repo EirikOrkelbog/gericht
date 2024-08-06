a2d75a752379cf1d6650aca1a4f72452399a33ba
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2024-08-06 20:14:53 +0200

    created component for large image

---
+++ components/LargeImage.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+function LargeImage() {
+	return (
+		<div className="relative p-4 max-w-xl">
+			<Image src="/hero.png" alt="" width={600} height={600}/>
+			<div className="h-2/3 w-2/3 bg-brand absolute top-0 right-0 -z-10"></div>
+			<div className="h-2/3 w-2/3 bg-brand absolute bottom-0 left-0 -z-10"></div>
+		</div>
+	)
+}
+
+export default LargeImage;
